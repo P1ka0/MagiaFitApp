0076e96879d07e8e7fc8bb5613b94c81ad4b6c65
Pika0 <bypikao@gmail.com>
2022-06-28 12:21:28 +0200

    Reparado Lista De Booker
    Se ha reparado la lista para que se muestren las actividades completas, el ListView sigue sin funcionar.

--- app/src/main/java/com/example/magiafitapp/MainActivity.kt
+++ app/src/main/java/com/example/magiafitapp/MainActivity.kt
@@ -1,68 +1,11 @@
 package com.example.magiafitapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.DocumentChange
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
-import com.google.firebase.firestore.QuerySnapshot
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-
-  //   private lateinit var recyclerView: RecyclerView
-   private lateinit var userArrayList: ArrayList<User>
-    private lateinit var myAdapter: MyAdapter
-   private lateinit var db : FirebaseFirestore
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-       // recyclerView = findViewById(R.id.recyclerView)
-       // recyclerView.layoutManager = LinearLayoutManager(this)
-       // recyclerView.setHasFixedSize(true)
-
-        userArrayList = arrayListOf()
-
-       recyclerView.adapter = myAdapter
-
-        EventChangeListener()
-
     }
-
-    private fun EventChangeListener(){
-
-        db = FirebaseFirestore.getInstance()
-        db.collection("User").
-                addSnapshotListener(object : EventListener<QuerySnapshot>{
-                    override fun onEvent(
-                        value: QuerySnapshot?,
-                        error: FirebaseFirestoreException?
-                    ) {
-                        if (error != null){
-
-                            Log.e("Firestore Error", error.message.toString())
-                            return
-                        }
-
-                        for (dc : DocumentChange in value?.documentChanges!!){
-
-                            if (dc.type == DocumentChange.Type.ADDED){
-
-                                userArrayList.add(dc.document.toObject(User::class.java))
-                            }
-                        }
-
-                        myAdapter.notifyDataSetChanged()
-                    }
-                })
-
-
-    }
-
 }
--- app/src/main/java/com/example/magiafitapp/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.magiafitapp
-
-data class User(var firstName: String ?= null,
-                var lastName: String ?= null,
-                var email: String ?= null
-
-)
